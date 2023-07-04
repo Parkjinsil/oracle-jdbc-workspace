cf013352f1fb1d8ae0d3e52fa596508eb7cde9ed
박진실 <wlstlf971017@naver.com>
2023-07-04 17:45:34 +0900

    feat0704

---
+++ 04_MemberMVC/src/com/kh/controller/MemberController.java
@@ -0,0 +1,105 @@
+package com.kh.controller;
+
+import java.sql.SQLException;
+
+import com.kh.model.dao.MemberDAO;
+import com.kh.model.vo.Member;
+
+public class MemberController {
+
+	private MemberDAO dao = new MemberDAO();
+	
+	public boolean joinMembership(Member m) {
+
+		// id가 없다면 회원가입 후 true 반환
+		// 없다면 false 값 반환
+		try {
+			if(dao.getMember(m.getId())!=null) {
+				return false;
+			} else {
+				dao.registerMember(m);
+				return true;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public String login(String id, String password) {
+
+		// 로그인 성공하면 이름 반환
+		// 실패하면 null 반환
+		try {
+			Member m = new Member();
+			m.setId(id);
+			m.setPassword(password);
+
+			Member result = dao.login(m);
+			if(result!=null) {
+				return result.getName();
+			}
+			
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public boolean changePassword(String id, String oldPw, String newPw) {
+
+		// 로그인 했을 때 null이 아닌 경우
+		// 비밀번호 변경 후 true 반환, 아니라면 false 반환
+		try {
+			Member m = new Member();
+			m.setId(id);
+			m.setPassword(oldPw);
+
+			Member result = dao.login(m);
+			if(result!=null) {
+				result.setPassword(newPw);
+				dao.updatePassword(result);
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		
+		return false;
+	}
+	
+	public void changeName(String id, String name) {
+
+		// 이름 변경!
+		try {
+			Member m = new Member();
+			m.setId(id);
+			m.setName(name);
+			
+			dao.updateName(m);
+			System.out.println("이름변경");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
